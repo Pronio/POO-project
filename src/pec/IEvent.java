c9e606730ab39649ba54f4c448a834e5495eead7
Jowjowf <joseff1911@gmail.com>
2018-05-10 03:59:40 +0100

    PEC package Comments
    Comments for JavaDoc

--- src/pec/IEvent.java
+++ src/pec/IEvent.java
@@ -1,6 +1,24 @@
 package pec;
 
+/**
+ * Interface representing the contract to be used
+ * by classes of other packages to interact
+ * with a general event. Assumes that a variable
+ * representing the time-stamp is added
+ * in inheriting classes
+ *
+ */
 public interface IEvent extends Comparable<IEvent>{
+	/**
+	 * Run an event main logic 
+	 * @return a new event of the same type
+	 * with new parameters
+	 */
 	public IEvent[] execute();
+	
+	/**
+	 * Get the time-stamp variable
+	 * @return the variable representing the time-stamp  
+	 */
 	public double getTime(); 
 }
--- src/pec/PEC.java
+++ src/pec/PEC.java
@@ -2,6 +2,11 @@ package pec;
 
 import java.util.LinkedList;
 
+/**
+ * PEC class that holds the collection of events.
+ * The collection is represented by a LinkedList.
+ *
+ */
 public class PEC implements IPEC{
 	
 	LinkedList<IEvent> eventList;
@@ -10,6 +15,10 @@ public class PEC implements IPEC{
 		this.eventList = new LinkedList<IEvent>();
 	}
 	
+	/**
+	 * Ordered insertion in LinkedList.
+	 * Uses add method from LinkedList to insert.
+	 */
 	public void Add(IEvent e){
 		//System.out.println("Adding: "+e.toString()); 
 		if(eventList.isEmpty()){
@@ -17,6 +26,7 @@ public class PEC implements IPEC{
 		}else{
 			int i;
 			boolean found = false;
+			//Change to ListIterator
 			for(i=0; i<eventList.size(); i++){
 				if(e.compareTo(eventList.get(i)) < 0){
 					found = true;
@@ -31,6 +41,11 @@ public class PEC implements IPEC{
 			}
 		}
 	}
+	
+	/**
+	 * Calls remove method from LinkedList class with argument 0
+	 * @return the IEvent at the first position of the LinkedList
+	 */
 	public IEvent Remove(){
 		//System.out.println("Removing: "+eventList.getFirst().toString());
 		if(!eventList.isEmpty())
